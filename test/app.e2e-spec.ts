57c7b2bb3e4d42ef052ae0d68bdd63790caa974f
DLGuzBuho <danielguzmanbuho23@gmail.com>
2026-09-06 10:50:00 -0400

    fix: resolve jest types, supertest import, and tsconfig settings

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import supertest = require('supertest');
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -15,8 +15,12 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  })
+
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return supertest(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
